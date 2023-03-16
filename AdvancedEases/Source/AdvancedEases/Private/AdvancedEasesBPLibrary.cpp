d99b7d47cb93bf6202ef66c90c008ad6d4348888
DESKTOP-DF1Q9MS\Fafhrd <davel@wevr.com>
2023-03-16 12:48:54 -0700

    Initial commit of plugin code and binaries

---
+++ AdvancedEases/Source/AdvancedEases/Private/AdvancedEasesBPLibrary.cpp
@@ -0,0 +1,182 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "AdvancedEasesBPLibrary.h"
+#include "AdvancedEases.h"
+
+float UAdvancedEasesBPLibrary::AdvancedEase_Float(EAdvancedEases EaseType, float Delta, float Start, float End)
+{
+    float easeDelta = End - Start;
+
+    switch (EaseType)
+    {
+    case EAdvancedEases::AE_INBACK:
+    {
+        return easeDelta * EaseInBack(Delta) + Start;
+    }
+    case EAdvancedEases::AE_OUTBACK:
+    {
+        return easeDelta * EaseOutBack(Delta) + Start;
+    }
+    case EAdvancedEases::AE_INOUTBACK:
+    {
+        return easeDelta * EaseInOutBack(Delta) + Start;
+    }
+    case EAdvancedEases::AE_INBOUNCE:
+    {
+        return easeDelta * EaseInBounce(Delta) + Start;
+    }
+    case EAdvancedEases::AE_OUTBOUNCE:
+    {
+        return easeDelta * EaseOutBounce(Delta) + Start;
+    }
+    case EAdvancedEases::AE_INOUTBOUNCE:
+    {
+        return easeDelta * EaseInOutBounce(Delta) + Start;
+    }
+    case EAdvancedEases::AE_INELASTIC:
+    {
+        return easeDelta * EaseInElastic(Delta) + Start;
+    }
+    case EAdvancedEases::AE_OUTELASTIC:
+    {
+        return easeDelta * EaseOutElastic(Delta) + Start;
+    }
+    case EAdvancedEases::AE_INOUTELASTIC:
+    {
+        return easeDelta * EaseInOutElastic(Delta) + Start;
+    }
+    default:
+    {
+        return easeDelta * Delta + Start;
+    }
+    }
+}
+
+FVector UAdvancedEasesBPLibrary::AdvancedEase_Vector(EAdvancedEases EaseType, FVector Start, FVector End, float Delta)
+{
+    FVector easeDelta = End - Start;
+
+    return (easeDelta * AdvancedEase_Float(EaseType, Delta)) + Start;
+}
+
+float UAdvancedEasesBPLibrary::EaseInBack(float Delta)
+{
+    const float c1 = 1.70158;
+    const float c3 = c1 + 1;
+
+    return c3 * Delta * Delta * Delta - c1 * Delta * Delta;
+}
+
+float UAdvancedEasesBPLibrary::EaseOutBack(float Delta)
+{
+    const float c1 = 1.70158;
+    const float c3 = c1 + 1;
+
+    return 1 + c3 * pow(Delta - 1, 3) + c1 * pow(Delta - 1, 2);
+}
+
+float UAdvancedEasesBPLibrary::EaseInOutBack(float Delta)
+{
+    const float c1 = 1.70158;
+    const float c2 = c1 * 1.525;
+
+    if (Delta < 0.5)
+    {
+        return (pow(2 * Delta, 2) * ((c2 + 1) * 2 * Delta - c2)) / 2;
+    }
+    else
+    {
+        return (pow(2 * Delta - 2, 2) * ((c2 + 1) * (Delta * 2 - 2) + c2) + 2) / 2;
+    }
+}
+
+float UAdvancedEasesBPLibrary::EaseInBounce(float Delta)
+{
+    return 1 - EaseOutBounce(1 - Delta);
+}
+
+float UAdvancedEasesBPLibrary::EaseOutBounce(float Delta)
+{
+    const float n1 = 7.5625;
+    const float d1 = 2.75;
+
+    if (Delta < (1 / d1))
+    {
+        return n1 * Delta * Delta;
+    }
+    else if (Delta < (2 / d1))
+    {
+        return n1 * (Delta -= 1.5 / d1) * Delta + 0.75;
+    }
+    else if (Delta < (2.5 / d1))
+    {
+        return n1 * (Delta -= 2.25 / d1) * Delta + 0.9375;
+    }
+    else
+    {
+        return n1 * (Delta -= 2.625 / d1) * Delta + 0.984375;
+    }
+}
+
+float UAdvancedEasesBPLibrary::EaseInOutBounce(float Delta)
+{
+    if (Delta < 0.5)
+    {
+        return ((1 - EaseOutBounce(1 - 2 * Delta)) / 2);
+    }
+    else
+    {
+        return ((1 + EaseOutBounce(2 * Delta - 1)) / 2);
+    }
+}
+
+float UAdvancedEasesBPLibrary::EaseInElastic(float Delta)
+{
+    const float c4 = (2 * PI) / 3;
+
+    if (Delta > 0 && Delta < 1)
+    {
+        return -pow(2, 10 * Delta - 10) * sin((Delta * 10 - 10.75) * c4);
+    }
+    else
+    {
+        return Delta;
+    }
+}
+
+float UAdvancedEasesBPLibrary::EaseOutElastic(float Delta)
+{
+    const float c4 = (2 * PI) / 3;
+
+    if (Delta > 0 && Delta < 1)
+    {
+        return pow(2, -10 * Delta) * sin((Delta * 10 - 0.75) * c4) + 1;
+    }
+    else
+    {
+        return Delta;
+    }
+}
+
+float UAdvancedEasesBPLibrary::EaseInOutElastic(float Delta)
+{
+    const float c5 = (2 * PI) / 4.5;
+
+    if (0 < Delta && Delta < 1)
+    {
+        if (Delta < 0.5)
+        {
+            return -(pow(2, 20 * Delta - 10) * sin((20 * Delta - 11.125) * c5)) / 2;
+        }
+        else
+        {
+            return (pow(2, -20 * Delta + 10) * sin((20 * Delta - 11.125) * c5)) / 2 + 1;
+        }
+    }
+    else
+    {
+        return Delta;
+    }
+}
+
+
---
+++ AdvancedEases/Source/AdvancedEases/Public/AdvancedEasesBPLibrary.h
@@ -0,0 +1,74 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "AdvancedEasesBPLibrary.generated.h"
+
+/* 
+*	Function library class.
+*	Each function in it is expected to be static and represents blueprint node that can be called in any blueprint.
+*
+*	When declaring function you can define metadata for the node. Key function specifiers will be BlueprintPure and BlueprintCallable.
+*	BlueprintPure - means the function does not affect the owning object in any way and thus creates a node without Exec pins.
+*	BlueprintCallable - makes a function which can be executed in Blueprints - Thus it has Exec pins.
+*	DisplayName - full name of the node, shown when you mouse over the node and in the blueprint drop down menu.
+*				Its lets you name the node using characters not allowed in C++ function names.
+*	CompactNodeTitle - the word(s) that appear on the node.
+*	Keywords -	the list of keywords that helps you to find node when you search for it using Blueprint drop-down menu. 
+*				Good example is "Print String" node which you can find also by using keyword "log".
+*	Category -	the category your node will be under in the Blueprint drop-down menu.
+*
+*	For more info on custom blueprint nodes visit documentation:
+*	https://wiki.unrealengine.com/Custom_Blueprint_Node_Creation
+*/
+
+UENUM(BlueprintType)
+enum class EAdvancedEases : uint8
+{
+	AE_INBACK		UMETA(DisplayName = "Ease In Back"),
+	AE_OUTBACK		UMETA(DisplayName = "Ease Out Back"),
+	AE_INOUTBACK	UMETA(DisplayName = "Ease InOut Back"),
+	AE_INELASTIC	UMETA(DisplayName = "Ease In Elastic"),
+	AE_OUTELASTIC	UMETA(DisplayName = "Ease Out Elastic"),
+	AE_INOUTELASTIC	UMETA(DisplayName = "Ease InOut Elastic"),
+	AE_INBOUNCE		UMETA(DisplayName = "Ease In Bounce"),
+	AE_OUTBOUNCE	UMETA(DisplayName = "Ease Out Bounce"),
+	AE_INOUTBOUNCE	UMETA(DisplayName = "Ease In Out Bounce"),
+	AE_LERP			UMETA(DisplayName = "Linear")
+};
+
+
+UCLASS()
+class UAdvancedEasesBPLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintPure, meta = (DisplayName = "Advanced Ease (Float)"), Category = "Interpolation")
+		static float AdvancedEase_Float(EAdvancedEases EaseType, float Delta, float Start = 0.0f, float End = 1.0f);
+	UFUNCTION(BlueprintPure, meta = (DisplayName = "Advanced Ease (Vector)"), Category = "Interpolation")
+		static FVector AdvancedEase_Vector(EAdvancedEases EaseType, FVector Start, FVector End, float Delta);
+
+private:
+
+	static float EaseInBack(float Delta);
+
+	static float EaseOutBack(float Delta);
+
+	static float EaseInOutBack(float Delta);
+
+	static float EaseInBounce(float Delta);
+
+	static float EaseOutBounce(float Delta);
+
+	static float EaseInOutBounce(float Delta);
+
+	static float EaseInElastic(float Delta);
+
+	static float EaseOutElastic(float Delta);
+
+	static float EaseInOutElastic(float Delta);
+
+};
